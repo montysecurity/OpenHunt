5cd8d9c7a8d2a57217a7064946e7bd8956bb61e7
montysecurity <montysecurity@protonmail.com>
2022-10-06 22:07:43 -0700

    adding kql

--- hunting.py
+++ hunting.py
@@ -1,5 +1,9 @@
+from unicodedata import name
 from shodan import Shodan
 import vt
+
+global_kql = 1
+global_list = 1
 
 def shodan():
         api = Shodan(str(input("Shodan API Key: ")))
@@ -13,18 +17,50 @@ def shodan():
             ipv4(ioc)
 
 def vt():
-    import vt, requests
+
+    def vt_kql(names, ):
+        names = str(names).replace("[", "(")
+        names = str(names).replace("]", ")")
+        names = str(names).replace(" '", " @\"")
+        names = str(names).replace("'", "\"")
+        print("search in (DeviceFileEvents, DeviceProcessEvents) | where InitiatingProcessFileName in~ " + str(names) + " or FileName in~ " + str(names))
+
+    import vt, requests, json
     objectAttrs = ["md5", "sha1", "sha256", "names"]
-    client = vt.Client(input("VTKEY: "))
+    #client = vt.Client(input("VTKEY: "))
     file = client.get_object("/files/163351e912ba5f7ca674f6017be509eb502be223032b93d89ae538ddc92df9fc")
-    for objectAttr in objectAttrs:
-        print(file.get(objectAttr))
-    url = "https://www.virustotal.com/api/v3/files/163351e912ba5f7ca674f6017be509eb502be223032b93d89ae538ddc92df9fc/dropped_files?limit=10"
+    md5 = file.get("md5")
+    sha1 = file.get("sha1")
+    sha256 = file.get("sha256")
+    names = file.get("names")
+    print(md5)
+    #print(names)
+    vt_kql(names)
+
+    
+
+    #for objectAttr in objectAttrs:
+        #print(objectAttr)
+        #if objectAttr == "names":
+         #   if len(result) > 1:
+          #      print("NAMES:")
+           #     for name in result:
+            #        print("- " + str(name))
+            #elif len(result) == 1:
+             #   print("NAME: "+ str(result).strip())
+        #if objectAttr != "names":
+         #   print(str(objectAttr).upper() + ": " + str(file.get(objectAttr)))
+    
+    url = "https://www.virustotal.com/api/v3/files/163351e912ba5f7ca674f6017be509eb502be223032b93d89ae538ddc92df9fc/dropped_files?limit=100"
     headers = {
     "accept": "application/json",
-    "x-apikey": str(client)
+    "x-apikey": ""
     }
     response = requests.get(url, headers=headers)
-    print(response)
+    #print(response.)
+    j = json.loads(str(response.text))
+    print("DROPPED FILES (SHA256): ")
+    for i in range(0,int(j["meta"]["count"])):
+        print(j["data"][int(i)]["id"])
 
 vt()
