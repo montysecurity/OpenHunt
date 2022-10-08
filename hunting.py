484f83f85a9563da9e0ae13438c8b22572379f0b
montysecurity <montysecurity@protonmail.com>
2022-10-07 23:13:20 -0700

    update

--- hunting.py
+++ hunting.py
@@ -1,9 +1,33 @@
+from turtle import pen
 from unicodedata import name
 from shodan import Shodan
 import vt
 
 global_kql = 1
 global_list = 1
+gloabal_sigmaIocRule ="""
+title: Auto-Generated IOC Rule
+id:
+status: experimental
+description: Detects artifacts related to IOC
+author: SOC Companion
+date: 
+references: 
+logsource:
+    product:
+    service:
+detection:
+    selection:
+        ParentImage|endswith:
+            - ReplaceMe
+        Image|endswith:
+            - ReplaceMe
+        Hashes|contains:
+            MD5=
+            SHA1=
+            SHA256=
+
+"""
 
 def shodan():
         api = Shodan(str(input("Shodan API Key: ")))
@@ -17,26 +41,19 @@ def shodan():
             ipv4(ioc)
 
 def vt():
-
-    def vt_kql(names, ):
-        names = str(names).replace("[", "(")
-        names = str(names).replace("]", ")")
-        names = str(names).replace(" '", " @\"")
-        names = str(names).replace("'", "\"")
-        print("search in (DeviceFileEvents, DeviceProcessEvents) | where InitiatingProcessFileName in~ " + str(names) + " or FileName in~ " + str(names))
-
     import vt, requests, json
     objectAttrs = ["md5", "sha1", "sha256", "names"]
     #client = vt.Client(input("VTKEY: "))
+    client = vt.Client("")
     file = client.get_object("/files/163351e912ba5f7ca674f6017be509eb502be223032b93d89ae538ddc92df9fc")
     md5 = file.get("md5")
     sha1 = file.get("sha1")
     sha256 = file.get("sha256")
     names = file.get("names")
-    print(md5)
     #print(names)
-    vt_kql(names)
-
+    print("---------------- IOC NAMES & HASHES --------------------")
+    names = str(names).replace("[", "(").replace("]", ")").replace(" '", " @\"").replace("'", "\"")
+    print("search in (DeviceProcessEvents, DeviceFileEvents) MD5 =~ {md5} or SHA1 =~ {sha1} or SHA256 =~ {sha256}")
     
 
     #for objectAttr in objectAttrs:
@@ -50,17 +67,18 @@ def vt():
              #   print("NAME: "+ str(result).strip())
         #if objectAttr != "names":
          #   print(str(objectAttr).upper() + ": " + str(file.get(objectAttr)))
-    
     url = "https://www.virustotal.com/api/v3/files/163351e912ba5f7ca674f6017be509eb502be223032b93d89ae538ddc92df9fc/dropped_files?limit=100"
     headers = {
     "accept": "application/json",
     "x-apikey": ""
     }
     response = requests.get(url, headers=headers)
-    #print(response.)
     j = json.loads(str(response.text))
-    print("DROPPED FILES (SHA256): ")
+    hashes = []
+    print("------------------- DROPPED FILES (SHA256) -----------------------")
     for i in range(0,int(j["meta"]["count"])):
-        print(j["data"][int(i)]["id"])
+        hashes.append(j["data"][int(i)]["id"])
+    hashes = str(hashes).replace("[", "(").replace("'", "\"").replace("]", ")")
+    print("search in (DeviceFileEvents, DeviceProcessEvents) InitiatingProcessSHA256 in~ " + str(hashes) + " or SHA256 in~ " + str(hashes))
 
 vt()
